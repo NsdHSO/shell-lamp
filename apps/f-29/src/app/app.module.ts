6d635e6860499b9d0ec24dfb2889003b49557a43
NsdHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-08-26 18:51:26 +0300

    feat: added ngx toaster

--- apps/f-29/src/app/app.module.ts
+++ apps/f-29/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { MatIconModule }           from "@angular/material/icon";
 import { BrowserModule }           from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { IconCoreModule }          from "ngx-liburg-icon";
+import { ToastrModule }            from "ngx-toastr";
 
 import { AppComponent }     from './app.component';
 import { RoutingAppModule } from "./routing-app.module";
@@ -19,6 +20,11 @@ import { TestComponent }    from './test/test.component';
     IconCoreModule,
     MatIconModule,
     BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
   ],
   providers    : [],
   bootstrap    : [ AppComponent ],
