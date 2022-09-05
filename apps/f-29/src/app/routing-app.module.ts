27abec8f08afe4f70994331e7d0346fee7705104
NsdHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-09-06 00:22:00 +0300

    refactor: remove redirect page

--- apps/f-29/src/app/routing-app.module.ts
+++ apps/f-29/src/app/routing-app.module.ts
@@ -19,7 +19,6 @@ const routes : Routes = [
     loadChildren : () => import('herus/Module').then ( m => m.AppModule ),
     canActivate  : [ AuthorizationGuard ]
   },
-  { path: '', pathMatch: 'full', redirectTo: '' },
 ];
 
 @NgModule ( {
