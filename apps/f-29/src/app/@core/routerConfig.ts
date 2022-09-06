030303baf9d9316b8c9250c3b7c042f07d3d5b8b
NsdHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-09-06 07:54:29 +0300

    feat: added frame whole libs

---
+++ apps/f-29/src/app/@core/routerConfig.ts
@@ -0,0 +1,15 @@
+export const CONFIG_MAIN = Object.freeze({
+  routerDataConfig: [
+    {
+      path: 'driver',
+      icon: 'fa_solid:arrow-down',
+      text: 'Driver',
+    },
+    {
+      path: 'truck',
+      icon: 'fa_solid:truck',
+      text: 'Truck',
+    },
+  ],
+  iconApp: '',
+});
--- apps/f-29/src/app/app.module.ts
+++ apps/f-29/src/app/app.module.ts
@@ -2,8 +2,10 @@ import { NgModule }                from '@angular/core';
 import { MatIconModule }           from "@angular/material/icon";
 import { BrowserModule }           from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FrameWholeModule }        from 'ngx-liburg-frame-side';
 import { IconCoreModule }          from "ngx-liburg-icon";
 import { ToastrModule }            from "ngx-toastr";
+import { CONFIG_MAIN }             from "./@core/routerConfig";
 
 import { AppComponent }     from './app.component';
 import { RoutingAppModule } from "./routing-app.module";
@@ -20,6 +22,7 @@ import { TestComponent }    from './test/test.component';
     IconCoreModule,
     MatIconModule,
     BrowserAnimationsModule,
+    FrameWholeModule.forRoot ( CONFIG_MAIN ),
     ToastrModule.forRoot({
       timeOut: 10000,
       positionClass: 'toast-top-left',
